6f63d4602cedc8313e0c3ba5e4e666fa69c85dc6
lunainde <lunainde@gmail.com>
2024-02-21 02:15:17 +0100

    pop-up/image

--- src/project.js
+++ src/project.js
@@ -1,5 +1,5 @@
 //console.log("test");+
-
+const hangmanImage = document.getElementById("hangmanImage");
 // Selecting HTML elements
 // const wordDisplay = document.getElementById('wordDisplay');
 // const hintDisplay = document.getElementById('hintDisplay');
@@ -13,6 +13,11 @@ let wordDisplay = []; // what user sees "_ _ _ _"
 
 // 1. F(X) START GAME =================
 function startGame() {
+  // Assure that game over screen is hidden
+  document.querySelector(".game-modal").style.display = "none";
+  document.querySelector(".game-win").style.display = "none";
+  // to do - reset the image to no body parts
+
   // Random selected item, word & hint from array
   const randomIndex = Math.floor(Math.random() * wordList.length);
   selectedWord = wordList[randomIndex].word.toUpperCase(); // declared outside (fut changes)
@@ -23,6 +28,11 @@ function startGame() {
   // Resets any game state variables, like the number of incorrect guesses.
   incorrectGuesses = 0;
   guessedLetters = [];
+  hangmanImage.src = "images/hm-0.png";
+  document.querySelectorAll("#keyboard button").forEach(button => {
+    button.disabled = false;
+  });
+  document.getElementById("incorrectDisplay").textContent = `Incorrect guesses: 0 | ${maxIncorrect}`;
 
   // Hidden word w/ underscore "_ _ _ _"
   // const arr = new Array(LEN).fill(0);
@@ -35,7 +45,6 @@ function startGame() {
 
   updateWordDisplay();
 }
-// startGame();
 
 //2. F(X) DISPLAY ON INTERFACE '_' & 'A'
 
@@ -57,6 +66,9 @@ function updateWordDisplay() {
     // INTERFACE: call display⬆ or displayWord(html) and append the letter of selected word.
     display.appendChild(letterElement);
   });
+  if(!wordDisplay.includes("_")) {
+    gameOver(true);
+  }
 }
 
 // 2. F(X) MANANGE KEYBOARD ==================
@@ -70,7 +82,7 @@ function keyboardClick(clickedLetter) {
   //clickedLetter is in lowercase for comparison
   clickedLetter = clickedLetter.toLowerCase();
   //check if clickLetter is in selectedWord
-  if (selectedWord.toLowerCase().includes(clickedLetter)) {    
+  if (selectedWord.toLowerCase().includes(clickedLetter)) {
     console.log("letter exists");
     //iterate through selectedWord:
     for (let i = 0; i < selectedWord.length; i++) {
@@ -81,15 +93,48 @@ function keyboardClick(clickedLetter) {
     // if yes connect to updateWordDisplay();
     updateWordDisplay();
   } else {
+    // if letter doesn't exist, update the incorrectGuesses & image
     // update incorrectGuesses VS guessedLetters
     incorrectGuesses++;
-    document.getElementById("incorrectDisplay").textContent = `Incorrect guesses: ${incorrectGuesses} | ${maxIncorrect}`;
-    // ==================upadate image
+    hangmanImage.src = `images/hm-${incorrectGuesses}.png`;
+    document.getElementById(
+      "incorrectDisplay"
+    ).textContent = `Incorrect guesses: ${incorrectGuesses} | ${maxIncorrect}`;
+
+    // document.getElementById("hangmanImage").src = "images/hm-0.png";
+    // document.getElementById("hangmanImage").src = "hm-" + incorrectGuesses + ".png";
+    if (incorrectGuesses === maxIncorrect) return gameOver(false);
   }
+
   // Add the clickedLetter to the guessedLetters array to keep track of it
   guessedLetters.push(clickedLetter);
   // Disable the clicked keyboard button so it can't be clicked again
   // setupKeyboard function
+}
+
+function gameOver(hasWon) {
+  console.log("game over call");
+  //display answer => selectedWord
+  document.getElementById("endGameWord").textContent = selectedWord;
+  // Define the sound file paths
+  const winSound = new Audio("audio/win-sound.wav");
+  const loseSound = new Audio("audio/game-over.wav");
+
+    if (hasWon) {
+      hangmanImage.src = "images/hm-win.png";
+      winSound.play();
+
+      setTimeout(function() {
+      document.querySelector(".game-win").style.display = "block"; // Show win modal
+      document.querySelector(".game-modal").style.display = "none"; // Hide lose modal if necessary
+      }, 2000);
+    } else {
+      loseSound.play();
+      
+      setTimeout(function() {
+      document.querySelector(".game-modal").style.display = "block";
+      }, 2000);
+    }
 }
 
 // assign events to all buttons
@@ -121,22 +166,22 @@ document.addEventListener("DOMContentLoaded", (event) => {
 // Dynamic Keyboard
 function generateKeyboard() {
   const keyboardDiv = document.getElementById("keyboard");
-  keyboardDiv.innerHTML = ''; // Clear existing buttons if any
+  keyboardDiv.innerHTML = ""; // Clear existing buttons if any
 
   // Generate A-Z
   for (let i = 65; i <= 90; i++) {
-      const button = document.createElement("button");
-      // Convert ASCII code to letter between 0 and 65535
-      button.textContent = String.fromCharCode(i); 
-      keyboardDiv.appendChild(button);
+    const button = document.createElement("button");
+    // Convert ASCII code to letter between 0 and 65535
+    button.textContent = String.fromCharCode(i);
+    keyboardDiv.appendChild(button);
   }
 
   // Generate 0-9
   for (let i = 0; i <= 9; i++) {
-      const button = document.createElement("button");
-      // Number to string
-      button.textContent = i.toString(); 
-      keyboardDiv.appendChild(button);
+    const button = document.createElement("button");
+    // Number to string
+    button.textContent = i.toString();
+    keyboardDiv.appendChild(button);
   }
 
   // Setup event listeners for the generated buttons
